95c669b5f13deba9db0af4bed306940683eb7f4d
AsadSaidi <asad.saidi.7e8@itb.cat>
2025-03-14 09:35:38 +0100

    14/03/2025

--- script.js
+++ script.js
@@ -31,17 +31,55 @@ class PosicionBarco{
  */
 function incializarBarcos(){
 
-    let barcosJSON = `{
-   { "name": "Portaaviones", "size": 5 },
-   { "name": "Acorazado", "size": 4 },
-   { "name": "Crucero", "size": 3 },
-   { "name": "Submarino", "size": 3 },
-   { "name": "Destructor", "size": 2 }
-    }`
+    let barcosJSON = `[
+        { "name": "Portaaviones", "size": 5 },
+        { "name": "Acorazado", "size": 4 },
+        { "name": "Crucero", "size": 3 },
+        { "name": "Submarino", "size": 3 },
+        { "name": "Destructor", "size": 2 }
+    ]`;
 
     let listaBarcos = JSON.parse(barcosJSON)
 
     for(let i = 0; i < 4; i++){
         Math.random
     }
+
+    listaBarcos.forEach(barco => {
+        console.log(`Nombre: ${barco.name}, Tamaño: ${barco.size}`);
+     });
+    
+     colocarBarcos(listaBarcos)
+}
+
+incializarBarcos()
+
+function incializarTablero(){
+    let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let letras = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+    let tableroHTML = document.getElementById(tablero);
+    let codigoHTML = "";
+
+    for(let i = 1; i < numeros.size; i++){
+        codigoHTML += "<tr>"
+        for(let j = 1; i < numeros.size; i++){
+            codigoHTML += `<td id="celda-${i}-${j}"`>
+        }
+    }
+}
+
+/**
+ * COLOCAR BARCOS:
+ */
+
+function colocarBarcos(listaBarcos){
+    let listaPosiciones = []
+    for(let i = 0; i < listaBarcos.size; i++){
+
+        listaBarcos.forEach( barco => {
+                
+        });
+    }
+    
+    
 }
