e034352d3b1fc90828e9e21bc27ab809f7a78085
AsadSaidi <asad.saidi.7e8@itb.cat>
2025-03-16 21:39:37 +0100

    Commit FInal: Tablero y Barcos puestos

--- script.js
+++ script.js
@@ -1,74 +1,54 @@
-/**
- * CLASES: Tablero, Barco, Celda
- */
-
-class Tablero{
-    tamanyo;
-    matrizCasillas;
-    listaBarcos;
-}
-
-class Barco{
-    nombre;
-    tamanyo;
-    listaPosiciones;
-    posicionesTocadas;
-}
-
-class Celda{
-    posicionX;
-    posicionY;
-    estado;
-}
-
-class PosicionBarco{
-    posicionX;
-    posicionY;
-}
-
-/**
- * INCIALIZAMOS POSCIONES DE LOS BARCOS
- */
-function incializarBarcos(){
-
-    let barcosJSON = `[
-        { "name": "Portaaviones", "size": 5 },
-        { "name": "Acorazado", "size": 4 },
-        { "name": "Crucero", "size": 3 },
-        { "name": "Submarino", "size": 3 },
-        { "name": "Destructor", "size": 2 }
-    ]`;
-
-    let listaBarcos = JSON.parse(barcosJSON)
-
-    for(let i = 0; i < 4; i++){
-        Math.random
+class Tablero {
+    constructor() {
+        this.tamanyo = 10;
+        this.matrizCasillas = [];
+        this.listaBarcos = [];
     }
-
-    listaBarcos.forEach(barco => {
-        console.log(`Nombre: ${barco.name}, Tamaño: ${barco.size}`);
-     });
-    
-     colocarBarcos(listaBarcos)
 }
 
-incializarBarcos()
+class Barco {
+    constructor(nombre, tamanyo, orientacion) {
+        this.nombre = nombre;
+        this.tamanyo = tamanyo;
+        this.orientacion = orientacion;
+        this.listaPosiciones = [];
+    }
+}
 
+class Celda {
+    constructor(posicionX, posicionY, estado) {
+        this.posicionX = posicionX;
+        this.posicionY = posicionY;
+        this.estado = estado;
+    }
+}
+
+class PosicionBarco {
+    constructor(posicionX, posicionY) {
+        this.posicionX = posicionX;
+        this.posicionY = posicionY;
+    }
+}
+
+/**
+ * INICIALIZAR TABLERO HTML
+ */
 function inicializarTablero() {
     let tableroHTML = document.getElementById("tablero");
     let contenido = "";
-    let listaLetras = [" ","A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-    let listaNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    let letras = [" ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+    let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+    contenido += "<tr>";
     for (let i = 0; i < 11; i++) {
-        contenido += `<th>${listaLetras[i]}</th>`; 
+        contenido += `<th>${letras[i]}</th>`;
     }
+    contenido += "</tr>";
 
     for (let i = 0; i < 10; i++) {
-        contenido += "<tr>";
-        contenido += `<tr><th>${listaNumeros[i]}</th>`;
-        for (let j = 0; j < 10; j++) { 
-            contenido += `<td id="celda-${i}-${j}"></td>`;
+        contenido += `<tr><th>${numeros[i]}</th>`;
+        for (let j = 0; j < 10; j++) {
+            contenido += `<td id="celda-${i}-${j}" class="celda"></td>`;
         }
         contenido += "</tr>";
     }
@@ -77,13 +57,94 @@ function inicializarTablero() {
 }
 
 /**
- * COLOCAR BARCOS:
+ * INICIALIZAR POSICIONES DE LOS BARCOS
  */
+function inicializarBarcos() {
+    let barcosJSON = `[ 
+        { "name": "Portaaviones", "size": 5, "orientacion": "vertical" },
+        { "name": "Acorazado", "size": 4, "orientacion": "vertical" },
+        { "name": "Crucero", "size": 3, "orientacion": "horizontal" },
+        { "name": "Submarino", "size": 3, "orientacion": "vertical" },
+        { "name": "Destructor", "size": 2, "orientacion": "horizontal" }
+    ]`;
 
-function colocarBarcos(listaBarcos){
-    let posX = Math.random()
-    
-    
+    let listaBarcosJSON = JSON.parse(barcosJSON);
+
+    let listaBarcos = listaBarcosJSON.map(barco =>
+        new Barco(barco.name, barco.size, barco.orientacion)
+    );
+
+    colocarBarcos(listaBarcos);
 }
 
-document.addEventListener("DOMContentLoaded", inicializarTablero);
+/**
+ * COLOCAR BARCOS:
+ */
+function colocarBarcos(listaBarcos) {
+    let tablero = document.getElementById("tablero");
+    let lineaMatriz = Array(10).fill()
+    let matrizOcupada = lineaMatriz.map(() => Array(10).fill(false));
+
+    tablero.style.backgroundColor = "#grey";
+
+    listaBarcos.forEach(barco => {
+        let colocado = false;
+        barco.listaPosiciones = [];
+
+        while (!colocado) {
+            let posX = Math.floor(Math.random() * 10);
+            let posY = Math.floor(Math.random() * 10);
+            let posicionesTemp = [];
+            let puedeColocar = true;
+
+            if (barco.orientacion === "horizontal") {
+            
+                if (posY + barco.tamanyo <= 10) {
+                    for (let i = 0; i < barco.tamanyo; i++) {
+                        if (matrizOcupada[posX][posY + i]) {
+                            puedeColocar = false;
+                            break;
+                        } else {
+                            posicionesTemp.push({ posicionX: posX, posicionY: posY + i });
+                        }
+                    }
+                } else {
+                    puedeColocar = false;
+                }
+            } else { // Vertical
+                if (posX + barco.tamanyo <= 10) {
+                    for (let i = 0; i < barco.tamanyo; i++) {
+                        if (matrizOcupada[posX + i][posY]) {
+                            puedeColocar = false;
+                            break;
+                        } else {
+                            posicionesTemp.push({ posicionX: posX + i, posicionY: posY });
+                        }
+                    }
+                } else {
+                    puedeColocar = false;
+                }
+            }
+
+            if (puedeColocar) {
+                
+                posicionesTemp.forEach(posicion => {
+                    matrizOcupada[posicion.posicionX][posicion.posicionY] = true;
+                    let celda = document.getElementById(`celda-${posicion.posicionX}-${posicion.posicionY}`);
+                    if (celda) {
+                        celda.style.backgroundColor = "gray"; 
+                        celda.innerText = barco.nombre[0].toUpperCase(); 
+                    }
+                });
+
+                barco.listaPosiciones = posicionesTemp;
+                colocado = true;
+            }
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    inicializarTablero();
+    inicializarBarcos();
+});
